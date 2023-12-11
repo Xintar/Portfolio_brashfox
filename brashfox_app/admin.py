ef32c682a37c8485b2de72867088684da9c390b6
andrzej <andszczepanski@gmail.com>
2023-12-11 14:26:27 +0100

    W trakcie

--- brashfox_app/admin.py
+++ brashfox_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from brashfox_app.models import (
+    BlogPost,
+    PostCategory,
+    PostComments,
+    Message
+)
+
 # Register your models here.
+admin.site.register(BlogPost)
+admin.site.register(PostCategory)
+admin.site.register(PostComments)
+admin.site.register(Message)
